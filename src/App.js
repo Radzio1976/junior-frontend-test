d92be028e675ddd3bebc73978728758e1672e2d3
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-08-24 14:00:28 +0200

    Added ProductsPage component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ProductsPage from './components/ProductsPage';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div id="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProductsPage />} />
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ProductsPage/ProductsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProductsPage extends React.Component {
+    render() {
+        return(
+            <h1>ProductsPage</h1>
+        )
+    }
+}
+
+export default ProductsPage;
